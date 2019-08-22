5d80e14460d0ec2114ead87a6866dd9c67726df6
vitor <vitorpaulasantos@gmail.com>
2019-08-22 15:14:54 -0300

    adicionando styles principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper className="App">
       <h1>Olá</h1>
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = Styled.div`
+  width: 1354px;
+  height: 1024px;
+  background: #663399;
+`;
 
 export default App;
